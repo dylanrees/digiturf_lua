672d39db334494e4992c57627e2d23e908047687
Dylan Rees <dylanrees@gmail.com>
2014-06-15 22:17:24 -0400

    Added player ability to steal territory, along with two new sound effects.

--- main.lua
+++ main.lua
@@ -121,17 +121,16 @@ function love.load()
     
     end
 		
-	--Instantiate the first player
+	--Instantiate the first players
 	mainplayer = Player.new("mainplayer", 4, 4, love.math.random(0,255), love.math.random(0,255), love.math.random(0,255))
 	mainplayer.control="human"
-	
 	cpu1 = Player.new("cpu1", 20, 21, love.math.random(0,255), love.math.random(0,255), love.math.random(0,255))
-	
 	tony = Player.new("tony", 30, 8, love.math.random(0,255), love.math.random(0,255), love.math.random(0,255))
 
- 
 	--Load sounds
 	 turfsound = love.audio.newSource("turfsound.wav", "static")
+	 stealsound = love.audio.newSource("stealsound.wav", "static")
+	 enemysound = love.audio.newSource("enemysound.wav", "static")
  
 end
 
@@ -141,30 +140,34 @@ function love.update(dt)
 
 	--timekeeping
 	gametime=gametime+1
-	if grabtime<grab_delay then grabtime=grabtime+1 end
 	-- helps with mouse referencing; gives block indices for mouse
 	mouse_x = math.floor(love.mouse.getX()/16)
 	mouse_y = math.floor(love.mouse.getY()/16)
  
 	--UPKEEP LOOP THAT APPLIES TO ALL PLAYERS
 	for i = 0, PlayerNumber do
-		if (PlayerList[i].grabtime<grab_delay) then PlayerList[i].grabtime=PlayerList[i].grabtime+1 end
+		if (PlayerList[i].grabtime<steal_delay) then PlayerList[i].grabtime=PlayerList[i].grabtime+1 end
 		--PlayerList[i].explore(PlayerList[i])
 	
-		if (PlayerList[i]~=mainplayer and PlayerList[i].grabtime==grab_delay and love.math.random(0,8)==1) then
+		if (PlayerList[i]~=mainplayer and PlayerList[i].grabtime>=grab_delay and love.math.random(0,8)==1) then
 			PlayerList[i].explore(PlayerList[i])
+			love.audio.play(enemysound)
 		end
 	
 	end
  
 	--routine for main player grabbing new tiles
-	if (love.mouse.isDown("l")==true and 
-		mainplayer.grabtime==grab_delay and 
-		mainplayer.isAdjacent(mainplayer,mouse_x,mouse_y)==true and 
-		OwnerGrid[mouse_x][mouse_y]=="nobody") then 
+	if (love.mouse.isDown("l")==true) then 
+		if (mainplayer.grabtime>=grab_delay and mainplayer.isAdjacent(mainplayer,mouse_x,mouse_y)==true and OwnerGrid[mouse_x][mouse_y]=="nobody") then
 			mainplayer.acquire(mainplayer,mouse_x,mouse_y)
 			mainplayer.grabtime=0
 			love.audio.play(turfsound)
+		end
+		if (mainplayer.grabtime==steal_delay and mainplayer.isAdjacent(mainplayer,mouse_x,mouse_y)==true and OwnerGrid[mouse_x][mouse_y]~="nobody" and OwnerGrid[mouse_x][mouse_y]~="mainplayer") then
+			mainplayer.acquire(mainplayer,mouse_x,mouse_y)
+			mainplayer.grabtime=0
+			love.audio.play(stealsound)
+		end
 	end
 		
 	-- random cell color-changing routine
@@ -184,7 +187,7 @@ function love.update(dt)
 	-- random color-mixing routine
 	for i = 0, xblocks-1 do
 		for j = 0, yblocks-1 do
-			CPweight=0.05 --dial this up or down to change the strength of the mixing
+			CPweight=0.007 --dial this up or down to change the strength of the mixing
 			ColorPalette={}; ColorPalette[0]=0; ColorPalette[1]=0; ColorPalette[2]=0
 			ColorPaletteNum=0 --these two variables are for weighted averaging of the colors surrounding a space
 			if (i>0) then if (OwnerGrid[i][j]~="nobody" and OwnerGrid[i-1][j]~="nobody") then ColorPaletteNum=ColorPaletteNum+1; for k=0, 2 do ColorPalette[k]=ColorPalette[k]+ColorGrid[i-1][j][k] end end end
@@ -218,8 +221,6 @@ function love.draw()
 		end
 	end
 	
-	
-	
 	--DRAW LOOP THAT APPLIES TO ALL PLAYERS
 	for k = 0, PlayerNumber do
 
@@ -230,7 +231,7 @@ function love.draw()
 				if (OwnerGrid[i][j]==PlayerList[k].name and (i==0 or (i>0 and OwnerGrid[i-1][j]~=PlayerList[k].name))) then love.graphics.line(i*16,j*16,i*16,j*16+16) end
 				if (OwnerGrid[i][j]==PlayerList[k].name and (i==xblocks-1 or (i<xblocks-1 and OwnerGrid[i+1][j]~=PlayerList[k].name))) then love.graphics.line(i*16+16,j*16,i*16+16,j*16+16) end
 				if (OwnerGrid[i][j]==PlayerList[k].name and (j==0 or (j>0 and OwnerGrid[i][j-1]~=PlayerList[k].name))) then love.graphics.line(i*16,j*16,i*16+16,j*16) end
-				if (OwnerGrid[i][j]==PlayerList[k].name and (j==yblocks-1 or (i<yblocks-1 and OwnerGrid[i][j+1]~=PlayerList[k].name))) then love.graphics.line(i*16,j*16+16,i*16+16,j*16+16) end
+				if (OwnerGrid[i][j]==PlayerList[k].name and (j==yblocks-1 or (j<yblocks-1 and OwnerGrid[i][j+1]~=PlayerList[k].name))) then love.graphics.line(i*16,j*16+16,i*16+16,j*16+16) end
 			end
 		end
 	end
